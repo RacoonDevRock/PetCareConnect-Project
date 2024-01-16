c99e3ec161f7b2492100da65471c08ed71dd4e03
Guillermo Sanchez Palacios <nothicc04@gmail.com>
2024-01-15 19:21:52 -0500

    Initial commit

---
+++ src/main/java/com/petcareconnect/api/PetCareConnectApplication.java
@@ -0,0 +1,13 @@
+package com.petcareconnect.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCareConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petcareconnect/api/PetCareConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petcareconnect.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
